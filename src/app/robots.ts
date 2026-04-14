7c6ded8ebb2e1bfedfe56557ad83ed833e3f835b
Maksim Dalen <maksamarskiy@gmail.com>
2026-04-13 22:26:22 -0500

    chore: disallow /account in robots.txt, add Stripe env vars to .env.example

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/", disallow: ["/api/"] },
+    rules: { userAgent: "*", allow: "/", disallow: ["/api/", "/account"] },
     sitemap: "https://getstroyka.com/sitemap.xml",
   };
 }
