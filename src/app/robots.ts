776d10616c487f5a4e88289d0308343db85797b4
Maksim Dalen <maksamarskiy@gmail.com>
2026-05-24 17:31:10 -0500

    feat(seo): disallow checkout endpoints in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,11 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/", disallow: ["/api/", "/account"] },
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/account", "/get-started/success", "/get-started/cancel"],
+    },
     sitemap: "https://getstroyka.com/sitemap.xml",
   };
 }
